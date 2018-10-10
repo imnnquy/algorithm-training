095fad6681b6fc06a4996b9e4c9961a32cd79ab3
Quy Nguyen <imnnquy@gmail.com>
2018-10-10 16:08:56 +0800

    Add books problem

--- Blue/Session 2/279B.py
+++ Blue/Session 2/279B.py
@@ -1,2 +1,34 @@
 # http://codeforces.com/problemset/problem/279/B
 
+n, t = map(int, input().split())
+a = list(map(int, input().split()))
+
+total_time = 0
+
+left = 0
+right = 0
+
+max_total_book = 0
+max_left = 0
+
+while True:
+
+    while right < n and total_time + a[right] <= t:
+        total_time += a[right]
+        right += 1
+
+    while total_time > t:
+        total_time -= a[left]
+        left += 1
+
+    if right - left > max_total_book:
+        max_total_book = right - left
+        max_left = left
+
+    if right < n:
+        total_time += a[right]
+        right += 1
+    else:
+        break
+
+print(max_total_book)
