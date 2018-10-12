0ec684ee4eac163edca165e0bcc4a7bb65d84ad9
Eric Quy Nguyen <nguyen.ngocquy@team.telstra.com>
2018-10-12 18:21:22 +0800

    Initiate 892B

--- Blue/Session 2/892B.py
+++ Blue/Session 2/892B.py
@@ -1,2 +1,22 @@
 # Problem from Codeforces
 # http://codeforces.com/problemset/problem/892/B
+
+n = int(input())
+L = list(map(int, input().split()))
+
+total_people = n
+start_to_kill = n - 1
+
+for i in range(n - 1, -1, -1):
+    if start_to_kill >= i - 1:
+        start_to_kill = i - 1
+    for j in range(start_to_kill, -1, -1):
+        if j >= i - L[i]:
+            total_people -= 1
+            start_to_kill = j - 1
+        else:
+            start_to_kill = j
+            break
+    if start_to_kill <= 0:
+        break
+print(total_people)
