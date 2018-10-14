65f485a43aaf3354d830b38e785600bd3bc7ce7d
Quy Nguyen <imnnquy@gmail.com>
2018-10-15 00:08:06 +0800

    add 892b solution

--- Blue/Session 2/892B.py
+++ Blue/Session 2/892B.py
@@ -5,18 +5,15 @@ n = int(input())
 L = list(map(int, input().split()))
 
 total_people = n
-start_to_kill = n - 1
-
-for i in range(n - 1, -1, -1):
-    if start_to_kill >= i - 1:
-        start_to_kill = i - 1
-    for j in range(start_to_kill, -1, -1):
-        if j >= i - L[i]:
-            total_people -= 1
-            start_to_kill = j - 1
+last_kill = 0
+for i in range(n - 1, 0, -1):
+    if L[i] > last_kill:
+        if L[i] < i:
+            total_people -= (L[i] - last_kill)
+            last_kill = L[i] - 1
         else:
-            start_to_kill = j
+            total_people -= i
             break
-    if start_to_kill <= 0:
-        break
+    else:
+        last_kill = last_kill - 1 if last_kill > 0 else 0
 print(total_people)
