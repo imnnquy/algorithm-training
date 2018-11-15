c156366896a6929fec236913d9a123dc79539512
Quy Nguyen <nguyen.ngocquy@team.telstra.com>
2018-11-15 15:43:02 +0800

    Add problem 448B

--- Blue/Session 5/Codeforces_448B.py
+++ Blue/Session 5/Codeforces_448B.py
@@ -39,3 +39,50 @@
 # Note
 # In the third sample you can act like that: first transform "both" into "oth" by removing the first character using the suffix automaton and then make two swaps of the string using the suffix array and get "hot".
 
+
+def check_solution(_s, _t):
+    is_array = False
+
+    s_map = [0] * 26
+    t_map = [0] * 26
+
+    s_length = len(_s)
+    t_length = len(_t)
+
+    for i in range(s_length):
+        s_map[ord(_s[i]) - 97] += 1
+
+    for i in range(t_length):
+        t_map[ord(_t[i]) - 97] += 1
+
+    for i in range(26):
+        if s_map[i] < t_map[i]:
+            return 'need tree'
+
+    if s_length == t_length:
+        return 'array'
+
+    s_left = 0
+    t_left = 0
+    while True:
+        if s_left >= s_length or t_left >= t_length:
+            break
+        if _s[s_left] != _t[t_left]:
+            s_left += 1
+        else:
+            s_left += 1
+            t_left += 1
+        if s_length - s_left < t_length - t_left:
+            is_array = True
+            break
+
+    if is_array:
+        return 'both'
+
+    return 'automaton'
+
+
+s = input()
+t = input()
+
+print(check_solution(s, t))
