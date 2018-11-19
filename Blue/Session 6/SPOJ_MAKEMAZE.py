9d0a080ffb933dc665f36b0ac59c9f456ccf87ea
Quy Nguyen <imnnquy@gmail.com>
2018-11-20 00:14:55 +0800

    Solve the maze

--- Blue/Session 6/SPOJ_MAKEMAZE.py
+++ Blue/Session 6/SPOJ_MAKEMAZE.py
@@ -5,9 +5,22 @@
 import queue
 
 
-def check_path(m, n, maze_matrix, start_point, end_point):
+def check_path(m, n, maze_graph, start_point, end_point):
+    visited = [[False for i in range(n)] for j in range(m)]
 
-    return True
+    q = queue.Queue()
+    visited[start_point[0]][start_point[1]] = True
+    q.put(start_point)
+    while not q.empty():
+        u = q.get()
+        for v in maze_graph[u[0]][u[1]]:
+            if not visited[v[0]][v[1]]:
+                if v[0] == end_point[0] and v[1] == end_point[1]:
+                    return True
+                visited[v[0]][v[1]] = True
+                q.put(v)
+
+    return False
 
 
 def get_edge_openings(m, n, maze_matrix):
@@ -39,7 +52,20 @@ def get_edge_openings(m, n, maze_matrix):
 def check_valid_maze(m, n, maze_matrix):
     edge_openings = get_edge_openings(m, n, maze_matrix)
     if len(edge_openings) == 2:
-        return check_path(m, n, maze_matrix, edge_openings[0], edge_openings[1])
+        maze_graph = []
+        for i in range(m):
+            maze_graph.append([[] for l in range(n)])
+            for j in range(n):
+                if maze_matrix[i][j] == 1:
+                    if i > 0 and maze_matrix[i - 1][j] == 1:
+                        maze_graph[i][j].append([i - 1, j])
+                    if j > 0 and maze_matrix[i][j - 1] == 1:
+                        maze_graph[i][j].append([i, j - 1])
+                    if i < m - 1 and maze_matrix[i + 1][j] == 1:
+                        maze_graph[i][j].append([i + 1, j])
+                    if j < n - 1 and maze_matrix[i][j + 1] == 1:
+                        maze_graph[i][j].append([i, j + 1])
+        return check_path(m, n, maze_graph, edge_openings[0], edge_openings[1])
     return False
 
 
@@ -55,6 +81,7 @@ def solution():
             rowi = input().strip()
             for col in range(n):
                 M[row].append(1 if rowi[col] == '.' else 0)
+
         result.append('valid' if check_valid_maze(m, n, M) else 'invalid')
 
     print(*result, sep='\n')
