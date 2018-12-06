ecaaf7c976f490e5f711f7ec34893a1ce1da0145
Quy Nguyen <imnnquy@gmail.com>
2018-12-06 22:57:34 +0800

    Use deleted list

--- Blue/Session 8 - Heap/Hackerrank_qheap1.py
+++ Blue/Session 8 - Heap/Hackerrank_qheap1.py
@@ -8,22 +8,30 @@ import heapq
 def solution():
     Q = int(input())
     my_list = []
+    delete_list = []
     for i in range(Q):
         query = input()
         if query.startswith('3'):
+            while True:
+                delete_list_length = len(delete_list)
+                contain_in_delete = False
+                for j in range(delete_list_length):
+                    if my_list[0] == delete_list[j]:
+                        heapq.heappop(my_list)
+                        delete_list[j] = delete_list[-1]
+                        delete_list.pop()
+                        contain_in_delete = True
+                        break
+                if not contain_in_delete:
+                    break
+
             print(my_list[0])
         else:
             command, param = map(int, query.split())
             if command == 1:
                 heapq.heappush(my_list, param)
             else:
-                for j in range(len(my_list)):
-                    if my_list[j] == param:
-                        my_list[j] = my_list[-1]
-                        my_list.pop()
-                        if j < len(my_list):
-                            heapq.heapify(my_list)
-                        break
+                delete_list.append(param)
 
 
 solution()
