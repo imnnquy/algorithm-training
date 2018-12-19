0ae8129981214458b1b7a51d67b805e99d87ae07
Quy Nguyen <imnnquy@gmail.com>
2018-12-20 00:16:47 +0800

    Lazy prog from spoj, TLE

--- Blue/Session 8 - Heap/SPOJ_LAZYPROG.py
+++ Blue/Session 8 - Heap/SPOJ_LAZYPROG.py
@@ -12,18 +12,42 @@ class Project:
         self.d = d
 
     def __lt__(self, other):
-        return True
+        return self.a > other.a
 
 
 def solution():
     t = int(input())
-    sum_money = 0
+
     for i in range(t):
+        sum_money = 0
+        initial_time = 0
+        priority_queue = []
+        projects = []
         n = int(input())
         for j in range(n):
             a, b, d = map(int, input().strip().split())
+            projects.append(Project(a, b, d))
+        projects.sort(key=lambda x: x.d, reverse=False)
 
-    print("{0:.2f}".format(sum_money))
+        for j in range(n):
+            initial_time += projects[j].b
+            heapq.heappush(priority_queue, projects[j])
+            if initial_time <= projects[j].d:
+                continue
+            while True:
+                pop_project = heapq.heappop(priority_queue)
+                if initial_time - pop_project.b <= projects[j].d:
+                    pop_project.b -= initial_time-projects[j].d
+                    sum_money += (initial_time-projects[j].d) / pop_project.a
+                    initial_time = projects[j].d
+                    heapq.heappush(priority_queue, pop_project)
+                    break
+
+                else:
+                    sum_money += pop_project.b / pop_project.a
+                    initial_time -= pop_project.b
+
+        print("{0:.2f}".format(sum_money))
 
 
 solution()
