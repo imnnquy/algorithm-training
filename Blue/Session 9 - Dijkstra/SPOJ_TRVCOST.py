afed940ea2c6b9b764af710bc2b9ea6079dea671
Quy Nguyen <imnnquy@gmail.com>
2019-01-12 19:15:31 +0800

    Revert changes

--- Blue/Session 9 - Dijkstra/SPOJ_TRVCOST.py
+++ Blue/Session 9 - Dijkstra/SPOJ_TRVCOST.py
@@ -6,6 +6,27 @@ import heapq
 import sys
 
 
+class input_tokenizer:
+    __tokens = None
+
+    def has_next(self):
+        return self.__tokens != [] and self.__tokens != None
+
+    def next(self):
+        token = self.__tokens[-1]
+        self.__tokens.pop()
+        return token
+
+    def __init__(self):
+        self.__tokens = sys.stdin.read().split()[::-1]
+
+
+inp = input_tokenizer()
+
+
+MAX = 1100
+
+
 class Node:
     def __init__(self, id, dist):
         self.dist = dist
@@ -15,9 +36,10 @@ class Node:
         return self.dist < other.dist
 
 
-def dijkstra(n, s, graph):
+def dijkstra(s, graph, queries):
 
-    dist = [-1 for x in range(n + 1)]
+    dist = [-1 for x in range(MAX)]
+    path = [-1 for y in range(MAX)]
     pqueue = []
     heapq.heappush(pqueue, Node(s, 0))
     dist[s] = 0
@@ -30,33 +52,41 @@ def dijkstra(n, s, graph):
             if w + neighbor.dist < dist[neighbor.id] or dist[neighbor.id] == -1:
                 dist[neighbor.id] = w + neighbor.dist
                 heapq.heappush(pqueue, Node(neighbor.id, dist[neighbor.id]))
+                path[neighbor.id] = u
 
+    results = []
+    nqueries = len(queries)
+    for i in range(nqueries):
+        if dist[queries[i]] != -1:
+            results.append(str(dist[queries[i]]))
+        else:
+            results.append('NO PATH')
 
-    return 1
+    return results
 
 
 def solution():
 
-    s = int(input())
+    N = int(inp.next())
 
-    cities_index = {}
+    graph = [[] for i in range(MAX)]
+    for i in range(N):
+        A = int(inp.next())
+        B = int(inp.next())
+        W = int(inp.next())
 
-    for i in range(s):
+        graph[A].append(Node(B, W))
+        graph[B].append(Node(A, W))
 
-        n = int(input())
-        graph = [[] for xx in range(n + 1)]
-        for xxx in range(n):
+    U = int(inp.next())
+    Q = int(inp.next())
+    queries = []
+    for i in range(Q):
+        queries.append(int(inp.next()))
 
-            cities_index[input()] = xxx + 1
+    results = dijkstra(U, graph, queries)
 
-            n_road = int(input())
-            for r in range(n_road):
-                ct, we = map(int, input().strip().split())
-                graph[xxx + 1].append(Node(ct, we))
-
-
-        results = dijkstra(U, graph, queries)
-
+    print(*results, sep='\n')
 
 
 solution()
