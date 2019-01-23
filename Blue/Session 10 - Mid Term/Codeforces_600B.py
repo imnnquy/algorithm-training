ba0ff451595761da217dde5635eba6439371dfd2
Quy Nguyen <nguyen.ngocquy@team.telstra.com>
2019-01-23 17:37:14 +0800

    use builtin binsect lib

--- Blue/Session 10 - Mid Term/Codeforces_600B.py
+++ Blue/Session 10 - Mid Term/Codeforces_600B.py
@@ -1,15 +1,7 @@
 # Problem from Codeforces
 # http://codeforces.com/problemset/problem/600/B
 
-
-def binary_search(arr, val, left, right):
-    if right > left + 1:
-        if val >= arr[(right - left) // 2 + left]:
-            return binary_search(arr, val, (right - left) // 2 + left, right)
-        else:
-            return binary_search(arr, val, left, (right - left) // 2 + left)
-
-    return right + 1 if (right < len(arr) and val >= arr[right]) else left + 1
+import bisect
 
 
 def solution():
@@ -20,8 +12,9 @@ def solution():
     a.sort()
 
     for i in range(m - 1):
-        print(binary_search(a, b[i], -1, n), end=' ')
-    print(binary_search(a, b[m-1], -1, n))
+        print(bisect.bisect_right(a, b[i]), end=' ')
+        # print(binary_search(a, b[i], -1, n), end=' ')
+    print(bisect.bisect_right(a, b[m - 1]))
 
 
 solution()
