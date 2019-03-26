dc8475d20785a4094cf4ab858d219fa69e641c8a
Quy Nguyen <imnnquy@gmail.com>
2019-03-27 00:37:13 +0800

    Fix wrong answers

--- Blue/Session 12 - Floyd-Warshall/UVA_10171.py
+++ Blue/Session 12 - Floyd-Warshall/UVA_10171.py
@@ -1,6 +1,7 @@
 # Problem from UVA
 # https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=508
-
+# import sys
+# sys.stdout = open("file.txt", "w+")
 
 INF = float(1e9)
 MAXN = 26
@@ -21,7 +22,16 @@ def floyd_warshall(youngGraph, oldGraph, start_young, start_old):
     if min_dist >= INF:
         print('You will never meet.')
     else:
-        print('{:d} {:s}'.format(min_dist, chr(ord('A') + min_i)))
+        flag = False
+        for i in range(MAXN):
+            d = youngGraph[start_young][i] + oldGraph[start_old][i]
+            if d == min_dist:
+                if flag:
+                    print(' ', end='')
+                else:
+                    flag = True
+                print('{:d} {:s}'.format(min_dist, chr(ord('A') + i)), end='')
+        print()
 
 
 def solution():
@@ -30,29 +40,36 @@ def solution():
         if N == 0:
             break
 
-        youngGraph = [[INF] * MAXN for i in range(MAXN)]
-        oldGraph = [[INF] * MAXN for i in range(MAXN)]
+        young_graph = [[INF] * MAXN for i in range(MAXN)]
+        old_graph = [[INF] * MAXN for i in range(MAXN)]
 
-        streets = []
+        for i in range(MAXN):
+            young_graph[i][i] = old_graph[i][i] = 0
+
         for i in range(N):
             line = list(map(str, input().strip().split()))
 
             if line[0] == 'Y':
                 x = ord(line[2]) - 65
                 y = ord(line[3]) - 65
-                youngGraph[x][y] = int(line[-1])
-                if line[1] == 'B':
-                    youngGraph[y][x] = int(line[-1])
+                if x != y:
+                    young_graph[x][y] = int(line[-1])
+                    if line[1] == 'B':
+                        young_graph[y][x] = int(line[-1])
             else:
                 x = ord(line[2]) - 65
                 y = ord(line[3]) - 65
-                oldGraph[x][y] = int(line[-1])
-                if line[1] == 'B':
-                    oldGraph[y][x] = int(line[-1])
+                if x != y:
+                    old_graph[x][y] = int(line[-1])
+                    if line[1] == 'B':
+                        old_graph[y][x] = int(line[-1])
 
         start_young, start_old = map(lambda x: ord(x) - 65, input().strip().split())
 
-        floyd_warshall(youngGraph, oldGraph, start_young, start_old)
+        if start_old == start_young:
+            print('{:d} {:s}'.format(0, chr(ord('A') + start_young)))
+        else:
+            floyd_warshall(young_graph, old_graph, start_young, start_old)
 
 
 solution()
