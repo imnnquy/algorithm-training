bea7c6abf4050004d648636c7fc6b6ba6a3fb4e3
Quy Nguyen <v-quynnguyen@expedia.com>
2019-06-06 21:48:58 +0800

    Solve MST from SPOJ

---
+++ Blue/Session 16 - Prim/SPOJ_MST.py
@@ -0,0 +1,63 @@
+# Problem from SPOJ
+# https://www.spoj.com/problems/MST/
+
+
+import heapq
+
+
+class Node:
+    def __init__(self, id, dist):
+        self.dist = dist
+        self.id = id
+
+    def __lt__(self, other):
+        return self.dist < other.dist
+
+
+def prim(N, graph):
+
+    dist = [-1 for x in range(N+1)]
+    path = [-1 for y in range(N+1)]
+    visited = [False for i in range(N + 1)]
+    pqueue = []
+    heapq.heappush(pqueue, Node(1, 0))
+    dist[1] = 0
+
+    while len(pqueue) > 0:
+        top = heapq.heappop(pqueue)
+        u = top.id
+        visited[u] = True
+        for neighbor in graph[u]:
+            v = neighbor.id
+            w = neighbor.dist
+            if not visited[v] and (w < dist[v] or dist[v] == -1):
+                dist[v] = w
+                heapq.heappush(pqueue, Node(v, w))
+                path[v] = u
+
+    result = 0
+    for i in range(1, N + 1):
+        if dist[i] != -1:
+            result += dist[i]
+
+    return result
+
+
+def solution():
+
+    N, M = map(int, input().split())
+
+    graph = [[] for i in range(N + 1)]
+    for i in range(M):
+        A, B, W = map(int, input().split())
+
+        graph[A].append(Node(B, W))
+        graph[B].append(Node(A, W))
+
+    result = prim(M, graph)
+
+    print(result)
+
+
+solution()
+
