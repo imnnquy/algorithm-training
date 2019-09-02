7adb5681b9c944537a73836cfb1f190efc44421b
Quy Nguyen <v-quynnguyen@expedia.com>
2019-09-02 13:18:31 +0800

    Solve Codeforces 468B

---
+++ Blue/Session 18 - DSU/Codeforces_468B.py
@@ -0,0 +1,69 @@
+# Problem from Codeforces
+# http://codeforces.com/problemset/problem/468/B
+
+
+parent = dict()
+ranks = dict()
+
+
+def make_set():
+    global parent, ranks
+    parent = dict()
+    ranks = dict()
+
+
+def find_set(u):
+    if parent.get(u) is not None and parent[u] is not u:
+        parent[u] = find_set(parent[u])
+    else:
+        parent[u] = u
+    return parent[u]
+
+
+def union_set(u, v):
+    up = find_set(u)
+    vp = find_set(v)
+    if ranks.get(up) is None:
+        ranks[up] = 1
+    if ranks.get(vp) is None:
+        ranks[vp] = 1
+
+    if ranks[up] > ranks[vp]:
+        parent[vp] = up
+    elif ranks[up] < ranks[vp]:
+        parent[up] = vp
+    else:
+        parent[up] = vp
+        ranks[vp] += 1
+
+
+def solution():
+    set_indicators = dict()
+    n, a, b = map(int, input().split())
+    numbers = list(map(int, input().split()))
+    A = n + 1
+    B = A + 1
+
+    make_set()
+    for i in range(n):
+        set_indicators[numbers[i]] = i
+
+    for i in range(n):
+        if set_indicators.get(a - numbers[i]) is not None:
+            union_set(i, set_indicators.get(a - numbers[i]))
+        else:
+            union_set(i, B)
+        if set_indicators.get(b - numbers[i]) is not None:
+            union_set(i, set_indicators.get(b - numbers[i]))
+        else:
+            union_set(i, A)
+    if find_set(A) == find_set(B):
+        print('NO')
+        return
+
+    print('YES')
+    for i in range(n):
+        print(1 if find_set(i) == find_set(n + 2) else 0)
+
+
+solution()
